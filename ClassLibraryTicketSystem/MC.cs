014958aa4861db09bc3b45fea2feeb2b25225dd5
Edem Beytullaev <edem0004@edu.easj.dk>
2021-09-29 22:35:11 +0200

    Task 2

---
+++ ClassLibraryTicketSystem/MC.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryTicketSystem
+{
+    public class MC
+    {
+
+
+        public string Licenseplate { get; set; }
+        public DateTime Date { get; set; }
+
+        /// <summary>
+        /// Return the price of motorcycle
+        /// </summary>
+        /// <returns> 125 </returns>
+        public double Price()
+        {
+            return 125;
+        }
+        /// <summary>
+        ///  Return the vehicle type of motorcycles
+        /// </summary>
+        /// <returns> return MC </returns>
+        public string VehicleType()
+        {
+            return "MC";
+        }
+
+    }
+}
---
+++ TicketTest/MCTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClassLibraryTicketSystem;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryTicketSystem.Tests
+{
+    [TestClass()]
+    public class MCTests
+    {
+        [TestMethod()]
+        public void PriceTest()
+        {
+            //Arrange  
+            MC mc = new MC();
+            //Act  
+            var expect = 125;
+            //Assert
+            Assert.AreEqual(expect, mc.Price());
+        }
+
+        [TestMethod()]
+        public void VehicleTypeTest()
+        {
+            //Arrange  
+            MC mc = new MC();
+            //Act  
+            var expect = "MC";
+            //Assert
+            Assert.AreEqual(expect, mc.VehicleType());
+        }
+    }
+}
